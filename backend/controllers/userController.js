3f999eda92ad92730e96eb801f27b7ba1fcc1fd6
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-09-07 14:25:09 +0100

    setting up the token and auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
-const bcryptjs = require('bcryptjs')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const registerUser = asyncHandler(async (req, res) => {
 
@@ -12,42 +13,71 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     //check if user Exist
-    const userExist = await User.findOne({email})
+    const userExist = await User.findOne({ email })
 
     if (userExist) {
         res.status(400)
         throw new Error("User already exist")
     }
 
-    const salt = await bcryptjs.genSalt(10)
-    const hashedpassword = await bcryptjs.hash(password, salt)
+    const salt = await bcrypt.genSalt(10)
+    const hashedpassword = await bcrypt.hash(password, salt)
 
     const user = await User.create({
         name,
         email,
         password: hashedpassword,
     })
-    
-    if(user){
+
+    if (user) {
         res.status(201).json({
-            _id : user._id,
-            name : user.name,
-            email : user.email
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
         })
-    }else{
+    } else {
         res.status(400)
         throw new Error("User data wrong")
     }
-
 })
-
-
 
 const loginUser = asyncHandler(async (req, res) => {
-    res.send("Login user")
+    const { email, password } = req.body
+
+    //check if user is alread registered
+    const user = await User.findOne({ email })
+
+    console.log(user, 'userss')
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('Bad Credential')
+    }
 })
+
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    }
+    res.status(200).json(user)
+})
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+}
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
