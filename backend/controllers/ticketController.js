68333b1dc10c6754bf9da727d2d76f0fee4209d2
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-09-13 21:33:39 +0100

    get ticket and create ticket support backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,15 +1,48 @@
 const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel")
+const Ticket = require("../models/ticketModel")
+
 
 
 const getTickets = asyncHandler(async (req, res) => {
-    
-    res.send({ message: "GetTickets" })
+    //check if it the user
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const tickets = await Ticket.find({user : req.user.id})
+
+    res.send(tickets)
 })
 
 
 const createTicket = asyncHandler(async (req, res) => {
-    res.send({ message: "CreateTicket" })
+
+    const {product,description} = req.body
+
+    if(!product || !description){
+        res.status(400)
+        throw new Error("Product or description is not filled")
+    }
+
+    const user = User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user : req.user.id,
+        status : "new"
+    })
+
+    res.status(201).json(ticket)
 })
 
 
