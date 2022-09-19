5c5a5a1a48fa1dd39da183e57ec206565ac018aa
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-09-19 22:45:25 +0100

    single ticket route

--- frontend/src/components/TicketItem.jsx
+++ frontend/src/components/TicketItem.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
+
 function TicketItem({ ticket }) {
   return (
     <div className="ticket items-center mb-5">
       <h3>{Date(1000).toString().slice(8, 21)}</h3>
       <h3> {ticket.product}</h3>
-      {/* <h3> {ticket.description}</h3> */}
-      <h3 className="btn btn-xs btn-disabled text-white bg-green-500 border-0"> {ticket.status}</h3>
-      <div className="btn btn-xs bg-white border-0 text-black">view</div>
+      <h3 className="btn btn-xs btn-disabled text-white bg-green-500 border-0">
+        {ticket.status}
+      </h3>
+
+      <Link to={`${ticket._id}`}>
+        <div className="btn btn-xs bg-white border-0 text-black">view</div>
+      </Link>
     </div>
   );
 }
